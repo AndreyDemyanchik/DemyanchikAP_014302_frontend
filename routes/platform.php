fff508e2cac9fc7ce179e0a888edec8558b90bb1
Демьянчик Андрей Павлович (demyanchik_ap) <demyanchik_ap@nlmk.com>
2023-11-13 22:28:00 +0300

    add platform routes

---
+++ routes/platform.php
@@ -0,0 +1,132 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Orchid\Screens\Examples\ExampleActionsScreen;
+use App\Orchid\Screens\Examples\ExampleCardsScreen;
+use App\Orchid\Screens\Examples\ExampleChartsScreen;
+use App\Orchid\Screens\Examples\ExampleFieldsAdvancedScreen;
+use App\Orchid\Screens\Examples\ExampleFieldsScreen;
+use App\Orchid\Screens\Examples\ExampleGridScreen;
+use App\Orchid\Screens\Examples\ExampleLayoutsScreen;
+use App\Orchid\Screens\Examples\ExampleScreen;
+use App\Orchid\Screens\Examples\ExampleTextEditorsScreen;
+use App\Orchid\Screens\PlatformScreen;
+use App\Orchid\Screens\Role\RoleEditScreen;
+use App\Orchid\Screens\Role\RoleListScreen;
+use App\Orchid\Screens\User\UserEditScreen;
+use App\Orchid\Screens\User\UserListScreen;
+use App\Orchid\Screens\User\UserProfileScreen;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use Tabuna\Breadcrumbs\Trail;
+use App\Orchid\Screens\ChooseCategoriesAndParamsForHandlingScreen;
+use App\Orchid\Screens\ChooseStatDataForVisualizationScreen;
+use App\Orchid\Screens\ChooseStatVisualizationScreen;
+use App\Orchid\Screens\DownloadPdfReportScreen;
+use App\Orchid\Screens\FormImgVisualizationScreen;
+use App\Orchid\Screens\GenerateReportScreen;
+use App\Orchid\Screens\MakeReportScreen;
+use App\Orchid\Screens\StartHandlingStatDataScreen;
+
+/*
+|--------------------------------------------------------------------------
+| Dashboard Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the need "dashboard" middleware group. Now create something great!
+|
+*/
+
+// Main
+Route::screen('/main', PlatformScreen::class)
+    ->name('platform.main')
+    ->middleware('redirectByRole');
+
+// Platform > Profile
+Route::screen('profile', UserProfileScreen::class)
+    ->name('platform.profile')
+    ->breadcrumbs(fn (Trail $trail) => $trail
+        ->parent('platform.index')
+        ->push(__('Profile'), route('platform.profile')));
+
+// Platform > System > Users > User
+Route::screen('users/{user}/edit', UserEditScreen::class)
+    ->name('platform.systems.users.edit')
+    ->breadcrumbs(fn (Trail $trail, $user) => $trail
+        ->parent('platform.systems.users')
+        ->push($user->name, route('platform.systems.users.edit', $user)));
+
+// Platform > System > Users > Create
+Route::screen('users/create', UserEditScreen::class)
+    ->name('platform.systems.users.create')
+    ->breadcrumbs(fn (Trail $trail) => $trail
+        ->parent('platform.systems.users')
+        ->push(__('Create'), route('platform.systems.users.create')));
+
+// Platform > System > Users
+Route::screen('users', UserListScreen::class)
+    ->name('platform.systems.users')
+    ->breadcrumbs(fn (Trail $trail) => $trail
+        ->parent('platform.index')
+        ->push(__('Users'), route('platform.systems.users')));
+
+// Platform > System > Roles > Role
+Route::screen('roles/{role}/edit', RoleEditScreen::class)
+    ->name('platform.systems.roles.edit')
+    ->breadcrumbs(fn (Trail $trail, $role) => $trail
+        ->parent('platform.systems.roles')
+        ->push($role->name, route('platform.systems.roles.edit', $role)));
+
+// Platform > System > Roles > Create
+Route::screen('roles/create', RoleEditScreen::class)
+    ->name('platform.systems.roles.create')
+    ->breadcrumbs(fn (Trail $trail) => $trail
+        ->parent('platform.systems.roles')
+        ->push(__('Create'), route('platform.systems.roles.create')));
+
+// Platform > System > Roles
+Route::screen('roles', RoleListScreen::class)
+    ->name('platform.systems.roles')
+    ->breadcrumbs(fn (Trail $trail) => $trail
+        ->parent('platform.index')
+        ->push(__('Roles'), route('platform.systems.roles')));
+
+// Example...
+Route::screen('example', ExampleScreen::class)
+    ->name('platform.example')
+    ->breadcrumbs(fn (Trail $trail) => $trail
+        ->parent('platform.index')
+        ->push('Example Screen'));
+
+Route::screen('/examples/form/fields', ExampleFieldsScreen::class)->name('platform.example.fields');
+Route::screen('/examples/form/advanced', ExampleFieldsAdvancedScreen::class)->name('platform.example.advanced');
+Route::screen('/examples/form/editors', ExampleTextEditorsScreen::class)->name('platform.example.editors');
+Route::screen('/examples/form/actions', ExampleActionsScreen::class)->name('platform.example.actions');
+
+Route::screen('/examples/layouts', ExampleLayoutsScreen::class)->name('platform.example.layouts');
+Route::screen('/examples/grid', ExampleGridScreen::class)->name('platform.example.grid');
+Route::screen('/examples/charts', ExampleChartsScreen::class)->name('platform.example.charts');
+Route::screen('/examples/cards', ExampleCardsScreen::class)->name('platform.example.cards');
+
+//Route::screen('idea', Idea::class, 'platform.screens.idea');
+
+// Routes of App
+Route::screen('handling/categories/params/choose', ChooseCategoriesAndParamsForHandlingScreen::class)
+    ->name('platform.chooseCategoriesAndParamsForHandling');
+Route::screen('statData/visualization/choose', ChooseStatDataForVisualizationScreen::class)
+    ->name('platform.chooseStatDataForVisualization');
+Route::screen('statData/visualization', ChooseStatVisualizationScreen::class)
+    ->name('platform.chooseStatVisualization');
+Route::screen('statData/visualization/start', StartHandlingStatDataScreen::class)
+    ->name('platform.startHandlingStatData');
+Route::screen('statData/visualization/img', FormImgVisualizationScreen::class)
+    ->name('platform.formImgVisualization');
+Route::screen('report/make', MakeReportScreen::class)
+    ->name('platform.makeReport');
+Route::screen('report/generate', GenerateReportScreen::class)
+    ->name('platform.generateReport');
+Route::screen('report/download', DownloadPdfReportScreen::class)
+    ->name('platform.downloadPdfReport');
