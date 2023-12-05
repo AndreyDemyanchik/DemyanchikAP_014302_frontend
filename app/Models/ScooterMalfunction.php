9592fe50a426c9096eaf75a8697ae6cf1bd94fdf
Демьянчик Андрей Павлович (demyanchik_ap) <demyanchik_ap@nlmk.com>
2023-12-05 17:38:30 +0300

    add models for visualizations

---
+++ app/Models/ScooterMalfunction.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ScooterMalfunction extends Model
+{
+    use HasFactory;
+
+    protected $table = 'scooter_malfunction';
+}
