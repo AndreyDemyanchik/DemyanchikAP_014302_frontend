f526ed77a1fb8bc0e61e059f66b72a3b71b5dec9
Демьянчик Андрей Павлович <demyanchik_ap@nlmk.com>
2023-12-12 22:50:23 +0300

    add default route prefix

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\VisualizationController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/admin');
+Route::get('statData/visualization/start', [
+    'as' => 'platform.startHandlingStatData',
+    'uses' => 'App\Orchid\Screens\StartHandlingStatDataScreen@__invoke',
+    'controller' => 'App\Orchid\Screens\StartHandlingStatDataScreen',
+    'prefix' => '/admin/'
+])->name('platform.startHandlingStatData');
